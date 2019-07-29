4a6e8dee57f79df691f2588bfc4a55b00e98e565
yanwei chen <1843008835@qq.com>
2019-07-29 21:05:46 +0900

    Add project files.

---
+++ ExcelAccess/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.file_TextBox = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.folder_TextBox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(766, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(227, 54)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "check"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 24
+        Me.ListBox1.Location = New System.Drawing.Point(31, 60)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(352, 628)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(587, 86)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 24)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "File"
+        '
+        'file_TextBox
+        '
+        Me.file_TextBox.Location = New System.Drawing.Point(766, 86)
+        Me.file_TextBox.Name = "file_TextBox"
+        Me.file_TextBox.Size = New System.Drawing.Size(405, 31)
+        Me.file_TextBox.TabIndex = 3
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(587, 144)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 24)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "folder"
+        '
+        'folder_TextBox
+        '
+        Me.folder_TextBox.Location = New System.Drawing.Point(766, 156)
+        Me.folder_TextBox.Name = "folder_TextBox"
+        Me.folder_TextBox.Size = New System.Drawing.Size(401, 31)
+        Me.folder_TextBox.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(587, 231)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(131, 24)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "model sheet"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(587, 326)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(160, 24)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "evidence sheet"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(766, 228)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(388, 32)
+        Me.ComboBox1.TabIndex = 11
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(766, 318)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(388, 32)
+        Me.ComboBox2.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1243, 704)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.folder_TextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.file_TextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents file_TextBox As TextBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label2 As Label
+    Friend WithEvents folder_TextBox As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+End Class
---
+++ ExcelAccess/Form1.vb
@@ -0,0 +1,91 @@
+﻿Imports Microsoft.Office.Interop
+Imports Microsoft.Office.Interop.Excel
+
+REM 需要下载插件Microsoft.Office.Interop.Excel
+Public Class Form1
+    Dim objexcelfile As Excel.Application
+    Dim objworkbook As Excel.Workbook
+    Dim objimportsheet As Excel.Worksheet
+    Dim intlastrownum, intcount As Integer
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim modelCollection As New Collection
+        Dim evidenceCollection As New Collection
+        Dim Mcount， Ecount As Integer
+        modelCollection = ReadExcel(file_TextBox.Text, ComboBox1.SelectedItem.ToString)
+        evidenceCollection = ReadExcel(file_TextBox.Text, ComboBox2.SelectedItem.ToString)
+
+        For Ecount = 1 To evidenceCollection.Count
+
+            For Mcount = 1 To modelCollection.Count
+                If evidenceCollection(Ecount) = modelCollection(Mcount) Then
+                    modelCollection.Remove(Mcount)
+                    Exit For
+                End If
+            Next
+        Next
+
+        For Mcount = 1 To modelCollection.Count
+            ListBox1.Items.Add(modelCollection(Mcount))
+        Next
+    End Sub
+
+    Private Sub file_TextBox_Click(sender As Object, e As EventArgs) Handles file_TextBox.Click
+        OpenFileDialog1.ShowDialog()
+        file_TextBox.Text = OpenFileDialog1.FileName
+    End Sub
+    Private Sub folder_TextBox_Click(sender As Object, e As EventArgs) Handles folder_TextBox.Click
+        FolderBrowserDialog1.ShowDialog()
+        folder_TextBox.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+
+    Function ReadExcel(filename As String, SheetName As String) As Collection
+        Dim resultCollection As New Collection
+
+        objexcelfile = New Excel.Application
+        objexcelfile.DisplayAlerts = False
+        objworkbook = objexcelfile.Workbooks.Open(filename)
+        objimportsheet = objworkbook.Sheets(SheetName)
+
+        intlastrownum = objimportsheet.UsedRange.Rows.Count
+
+        For intcount = 1 To intlastrownum
+            If objimportsheet.Cells(intcount, 1).value <> "" Then
+                resultCollection.Add(objimportsheet.Cells(intcount, 1).value)
+            End If
+        Next
+        CloseExcel(objexcelfile, objworkbook, objimportsheet)
+        Return resultCollection
+    End Function
+
+    Private Sub file_TextBox_TextChanged(sender As Object, e As EventArgs) Handles file_TextBox.TextChanged
+        Dim ShCount As Integer
+        objexcelfile = New Excel.Application
+        objexcelfile.DisplayAlerts = False
+        objworkbook = objexcelfile.Workbooks.Open(file_TextBox.Text)
+        For ShCount = 1 To objworkbook.Worksheets.Count
+            ComboBox1.Items.Add(objworkbook.Worksheets(ShCount).name)
+            ComboBox2.Items.Add(objworkbook.Worksheets(ShCount).name)
+        Next
+        CloseExcel(objexcelfile, objworkbook, objimportsheet)
+    End Sub
+
+    Private Sub CloseExcel(objexcelfile As Excel.Application, objworkbook As Excel.Workbook, objimportsheet As Excel.Worksheet)
+        Try
+            objworkbook = Nothing
+            objimportsheet = Nothing
+            objexcelfile = Nothing
+            objexcelfile.Quit()
+
+
+        Catch ex As Exception
+            Console.WriteLine(ex)
+
+        End Try
+
+        GC.Collect()
+        GC.WaitForPendingFinalizers()
+    End Sub
+End Class
